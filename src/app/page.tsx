946770d07b2c10e0f170e640a10137ecb94eb5a8
Sathwik Amburi <sathwik.amburi@gmail.com>
2024-09-05 21:04:15 +0200

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/Toggle";
+export default function Home() {
+  return (
+    <div className="">
+      <Button>Click me</Button>
+      <ModeToggle />
+    </div>
+  );
+}
